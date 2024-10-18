97594949b6ec3bdb7db8b9b6570fec63d7f9fd9b
Tincho Morilla <mmorilla@fi.uba.ar>
2024-10-18 15:41:26 -0300

    Up to now, we can get the new album releases using asynchronous methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+#[path = "spotify_api/spotify_token.rs"]
+mod spotify_token;
+
+use dotenv::dotenv;
+use reqwest;
+use reqwest::Error;
+use serde_json::Value;
+use spotify_token::SpotifyToken;
+use std::env; // Import the dotenv function
+
+async fn get_new_album_releases() -> Result<(), Error> {
+    dotenv().ok();
+    let client_id = env::var("SPOTIFY_CLIENT_ID").expect("Client ID not found");
+    let client_secret = env::var("SPOTIFY_CLIENT_SECRET").expect("Client Secret not found");
+    let token = SpotifyToken::new(&client_id, &client_secret).await?;
+    let url = "https://api.spotify.com/v1/browse/new-releases";
+    // Create a client
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(url)
+        .bearer_auth(token.get_access_token())
+        .send()
+        .await?;
+
+    // Parse the response body as JSON
+    let json: Value = response.json().await?;
+
+    // // Access the names of the albums
+    let albums = &json["albums"]["items"];
+    match albums.as_array() {
+        Some(albums_array) => {
+            for album in albums_array {
+                match album["name"].as_str() {
+                    Some(name) => println!("{}", name),
+                    None => println!("No name found"),
+                }
+            }
+        }
+        None => {
+            println!("No albums found");
+            return Ok(());
+        }
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    get_new_album_releases()
+        .await
+        .expect("Failed to get new album releases");
 }
