9fe6e5d0cca77a7e9060eb04827c0ec6c8a3c6bd
Tincho Morilla <mmorilla@fi.uba.ar>
2024-10-18 19:55:36 -0300

    Now we can get the new released albums tracks. By default, we obtain the first 20 albums.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,62 @@
 #[path = "spotify_api/spotify_token.rs"]
 mod spotify_token;
-
-use dotenv::dotenv;
-use reqwest;
-use reqwest::Error;
-use serde_json::Value;
 use spotify_token::SpotifyToken;
-use std::env; // Import the dotenv function
 
-async fn get_new_album_releases() -> Result<(), Error> {
-    dotenv().ok();
-    let client_id = env::var("SPOTIFY_CLIENT_ID").expect("Client ID not found");
-    let client_secret = env::var("SPOTIFY_CLIENT_SECRET").expect("Client Secret not found");
-    let token = SpotifyToken::new(&client_id, &client_secret).await?;
+#[path = "errors/type_of_errors.rs"]
+mod type_of_errors;
+use type_of_errors::Errors;
+
+#[path = "albums/album_information.rs"]
+mod album_information;
+use album_information::Album;
+
+use reqwest;
+use serde_json::json;
+use serde_json::Value;
+
+async fn get_new_album_tracks() -> Result<(), Errors> {
+    let albums = get_new_album_releases_ids().await?;
+    let token = SpotifyToken::new().await?;
+    let client = reqwest::Client::new();
+
+    let mut album_tracks_list = Vec::new();
+
+    for album in albums {
+        let url = format!(
+            "https://api.spotify.com/v1/albums/{}/tracks",
+            album.get_id()
+        );
+        let response = client
+            .get(&url)
+            .bearer_auth(token.get_access_token())
+            .send()
+            .await?;
+
+        let json: Value = response.json().await?;
+        let items = json["items"].as_array().ok_or(Errors::NoTracksFound)?;
+
+        let track_names: Vec<String> = items
+            .iter()
+            .filter_map(|item| item["name"].as_str().map(|name| name.to_string()))
+            .collect();
+
+        // Create a JSON object with the desired fields
+        let album_data = json!({
+            "album_name": album.get_name(),
+            "track_names": track_names
+        });
+
+        album_tracks_list.push(album_data);
+    }
+
+    let json_output = serde_json::to_string_pretty(&album_tracks_list)?;
+    println!("{}", json_output);
+
+    Ok(())
+}
+
+async fn get_new_album_releases_ids() -> Result<Vec<Album>, Errors> {
+    let token = SpotifyToken::new().await?;
     let url = "https://api.spotify.com/v1/browse/new-releases";
     // Create a client
     let client = reqwest::Client::new();
@@ -26,29 +70,27 @@ async fn get_new_album_releases() -> Result<(), Error> {
     // Parse the response body as JSON
     let json: Value = response.json().await?;
 
-    // // Access the names of the albums
+    // Access to the album fields
     let albums = &json["albums"]["items"];
-    match albums.as_array() {
-        Some(albums_array) => {
-            for album in albums_array {
-                match album["name"].as_str() {
-                    Some(name) => println!("{}", name),
-                    None => println!("No name found"),
-                }
+    let albums_array = albums.as_array().ok_or(Errors::NoAlbumsFound)?;
+    let mut albums = Vec::new();
+
+    for album in albums_array {
+        match (album["id"].as_str(), album["name"].as_str()) {
+            (Some(id), Some(name)) => {
+                let album = Album::new(name.to_string(), id.to_string());
+                albums.push(album);
             }
-        }
-        None => {
-            println!("No albums found");
-            return Ok(());
+            _ => println!("No ID or name found"),
         }
     }
 
-    Ok(())
+    Ok(albums)
 }
 
 #[tokio::main]
 async fn main() {
-    get_new_album_releases()
+    get_new_album_tracks()
         .await
-        .expect("Failed to get new album releases");
+        .expect("Error getting new album tracks");
 }
