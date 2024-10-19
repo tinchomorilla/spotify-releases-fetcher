ade37b4be93f0c70b4b07995c833889aa78e3ea2
Tincho Morilla <mmorilla@fi.uba.ar>
2024-10-19 02:18:01 -0300

    Changed the way i was making the post request. Now we make the post request in a sync way, because we only create the token once. We call the "initialize_token" function to make the post request and then when we want the access_token we call the "get_token" method.

--- src/spotify_api/spotify_token.rs
+++ src/spotify_api/spotify_token.rs
@@ -1,10 +1,10 @@
-use std::env;
-
 use base64::encode;
-use reqwest::Error;
-use serde::Deserialize;
 use dotenv::dotenv;
-
+use once_cell::sync::OnceCell;
+use serde::Deserialize;
+use serde_json::from_str;
+use std::env;
+use ureq;
 
 #[derive(Deserialize)]
 pub struct SpotifyToken {
@@ -12,29 +12,42 @@ pub struct SpotifyToken {
 }
 
 impl SpotifyToken {
-    pub async fn new() -> Result<Self, Error> {
+    fn new() -> Result<Self, Box<dyn std::error::Error>> {
         dotenv().ok();
-        let client_id = env::var("SPOTIFY_CLIENT_ID").expect("Client ID not found");
-        let client_secret = env::var("SPOTIFY_CLIENT_SECRET").expect("Client Secret not found");
-        let client = reqwest::Client::new();
+        let client_id = env::var("SPOTIFY_CLIENT_ID")?;
+        let client_secret = env::var("SPOTIFY_CLIENT_SECRET")?;
         let params = [("grant_type", "client_credentials")];
 
         // Encode client_id and client_secret
         let credentials = format!("{}:{}", client_id, client_secret);
         let encoded_credentials = encode(credentials);
 
-        let response = client
-            .post("https://accounts.spotify.com/api/token")
-            .header("Authorization", format!("Basic {}", encoded_credentials))
-            .form(&params)
-            .send()
-            .await?;
-
-        let token_response: SpotifyToken = response.json().await?;
+        let response = ureq::post("https://accounts.spotify.com/api/token")
+            .set("Authorization", &format!("Basic {}", encoded_credentials))
+            .send_form(&params)?;
+        let response_body = response.into_string()?;
+        let token_response: SpotifyToken = from_str(&response_body)?;
         Ok(token_response)
     }
 
     pub fn get_access_token(&self) -> &str {
         &self.access_token
     }
+}
+
+lazy_static::lazy_static! {
+    static ref TOKEN: OnceCell<SpotifyToken> = OnceCell::new();
+}
+
+pub fn initialize_token() -> Result<(), Box<dyn std::error::Error>> {
+    TOKEN.get_or_init(|| {
+        SpotifyToken::new().unwrap_or_else(|err| {
+            panic!("Failed to initialize SpotifyToken: {}", err);
+        })
+    });
+    Ok(())
+}
+
+pub fn get_token() -> &'static SpotifyToken {
+    TOKEN.get().expect("Token not initialized")
 }
