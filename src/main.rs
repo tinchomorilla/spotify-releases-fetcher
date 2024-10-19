60c2109732fb70acb2cedc265bb420736a1e52e8
Tincho Morilla <mmorilla@fi.uba.ar>
2024-10-19 00:29:16 -0300

    Good programming practices.

--- src/main.rs
+++ src/main.rs
@@ -32,24 +32,12 @@ async fn get_albums_tracks(albums: Vec<Album>) -> Result<String, Errors> {
     Ok(json_output)
 }
 
-async fn make_http_request(url: String) -> Result<reqwest::Response, Errors> {
-    let token = SpotifyToken::new().await?;
-    let client = reqwest::Client::new();
-
-    let response = client
-        .get(&url)
-        .bearer_auth(token.get_access_token())
-        .send()
-        .await?;
-    Ok(response)
-}
-
 async fn get_album_data(album: &Album) -> Result<Value, Errors> {
     let url = format!(
         "https://api.spotify.com/v1/albums/{}/tracks",
         album.get_id()
     );
-    let response = make_http_request(url).await?;
+    let response = make_http_get_request(url).await?;
     let json: Value = response.json().await?;
     let items = json["items"].as_array().ok_or(Errors::NoTracksFound)?;
     let track_names: Vec<String> = items
@@ -64,26 +52,17 @@ async fn get_album_data(album: &Album) -> Result<Value, Errors> {
 }
 
 async fn get_new_album_releases() -> Result<Vec<Album>, Errors> {
-    let token = SpotifyToken::new().await?;
     let url = "https://api.spotify.com/v1/browse/new-releases";
-    // Create a client
-    let client = reqwest::Client::new();
-
-    let response = client
-        .get(url)
-        .bearer_auth(token.get_access_token())
-        .send()
-        .await?;
+    let response = make_http_get_request(url.to_string()).await?;
 
     // Parse the response body as JSON
     let json: Value = response.json().await?;
 
-    // Access to the album fields
-    let albums = &json["albums"]["items"];
-    let albums_array = albums.as_array().ok_or(Errors::NoAlbumsFound)?;
+    let albums_json = &json["albums"]["items"];
+    let albums_array_json = albums_json.as_array().ok_or(Errors::NoAlbumsFound)?;
     let mut albums = Vec::new();
 
-    for album in albums_array {
+    for album in albums_array_json {
         match (album["id"].as_str(), album["name"].as_str()) {
             (Some(id), Some(name)) => {
                 let album = Album::new(name.to_string(), id.to_string());
@@ -96,16 +75,28 @@ async fn get_new_album_releases() -> Result<Vec<Album>, Errors> {
     Ok(albums)
 }
 
+async fn make_http_get_request(url: String) -> Result<reqwest::Response, Errors> {
+    let token = SpotifyToken::new().await?;
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(&url)
+        .bearer_auth(token.get_access_token())
+        .send()
+        .await?;
+    Ok(response)
+}
+
 #[tokio::main]
 async fn main() {
     let start = std::time::Instant::now();
     let albums = get_new_album_releases()
         .await
         .expect("Error getting new albums");
-    let tracks = get_albums_tracks(albums)
+    let albums_tracks = get_albums_tracks(albums)
         .await
         .expect("Error getting new albums tracks");
 
-    println!("{}", tracks);
+    println!("{}", albums_tracks);
     println!("Time elapsed: {:?}", start.elapsed());
 }
